eede4eac3591e17e1c4d55952a24b9917ca9b74f
Rishika <rig220@lehigh.edu>
2022-11-22 13:22:14 -0500

    Grading

--- Assignment 4.R
+++ Assignment 4.R
@@ -4,15 +4,17 @@ library(here)
 library(dplyr)
 library(ggplot2)
 install.packages('patchwork') 
-wot <- read_csv(here("Assignment 4","WOT.csv"))
+wot <- read_csv(here("WOT.csv"))
 wot <- filter(wot,Battles > 3)
 wot$WR <- as.numeric(gsub("%","",wot$WR))
-p1 <- ggplot(wot,aes(x=as.factor(Nation), y=WR)) + 
+# Failed to reorder the Nation and Class in plot : -10
+#Correct way to do it is below
+p1 <- ggplot(wot,aes(x=reorder(Nation,WR), y=WR)) + 
   geom_boxplot() + 
   labs(title = "Stats by Nation Played",
        x = "Nation Played",
        y = "Win Rate(WR)")
-p2 <- ggplot(wot,aes(x=as.factor(Class), y=WR)) + 
+p2 <- ggplot(wot,aes(x=reorder(Class,WR), y=WR)) + 
   geom_boxplot() + 
   labs(title = "Stats by Type Played",
        x = "Type Played",
